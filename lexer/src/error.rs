e85c02a5426c39d84864e911842e71ca59929ed2
Liao Junxuan <mikeljx@126.com>
2022-11-13 18:43:15 +0800

    lexer: add preprocessing

--- lexer/src/error.rs
+++ lexer/src/error.rs
@@ -1,12 +1,29 @@
-use strum_macros::Display;
+use std::{fmt::Display, path::PathBuf};
 
-struct Error {
-    line_num: usize,
-    error_type: ErrorKind,
+use colored::*;
+use indenter::indented;
+
+#[derive(Debug, thiserror::Error)]
+pub struct Error {
+    pub file_path: PathBuf,
+    pub line_num: usize,
+    pub error_type: ErrorKind,
 }
 
-#[derive(Debug, Display)]
-enum ErrorKind {
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{}: {}",
+            self.file_path.to_str().unwrap(),
+            self.line_num,
+            self.error_type
+        )
+    }
+}
+
+#[derive(Debug, strum_macros::Display)]
+pub enum ErrorKind {
     #[strum(serialize = "unterminated comment")]
     UnterminatedComment,
 }
--- lexer/src/main.rs
+++ lexer/src/main.rs
@@ -1,6 +1,67 @@
 mod error;
 mod preprocess;
 
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::{Path, PathBuf},
+};
+
+use anyhow::{anyhow, Context, Result};
+use clap::{builder::Str, Arg, Parser};
+
+#[derive(Parser)]
+struct Args {
+    /// Only run the preprocessor
+    #[arg(short = 'E')]
+    preprocessor_only: bool,
+    /// Treat the input as if it's already preprocessed
+    #[arg(long)]
+    preprocessed: bool,
+
+    #[arg(short, long)]
+    output: Option<String>,
+
+    file: String,
+}
+
+fn preprocessed_path(original_path: &Path) -> PathBuf {
+    let mut path = original_path.file_stem().unwrap().to_owned();
+    path.push(".i");
+    PathBuf::from(path)
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let file_path = Path::new(&args.file);
+    let mut file = File::open(&args.file)
+        .with_context(|| format!("cannot open \"{}\"", args.file))?;
+
+    if !args.preprocessed {
+        let mut src = String::new();
+        file.read_to_string(&mut src)?;
+        match preprocess::preprocess(src.chars()) {
+            Ok(preprocessed) => {
+                if args.preprocessor_only {
+                    let output_path = preprocessed_path(file_path);
+                    println!("{output_path:?}");
+                    let mut output = File::create(output_path)?;
+                    output.write_all(preprocessed.as_bytes())?;
+                    return Ok(())
+                }
+            }
+            Err(line_num) => {
+                return Err(error::Error {
+                    file_path: file_path.to_owned(),
+                    line_num,
+                    error_type: error::ErrorKind::UnterminatedComment,
+                })?
+            }
+        }
+    }
+    if args.preprocessor_only {
+        return Err(anyhow!("expect input to be not have been preprocessed"));
+    }
+
+    Ok(())
 }
